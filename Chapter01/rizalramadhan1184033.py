84f91e109cbb036240cc02084953f305c1199069
steward <izalr909@gmail.com>
2021-03-19 08:38:50 +0700

    1184033 Rizal Ramadhan

--- Chapter01/rizalramadhan1184033.py
+++ Chapter01/rizalramadhan1184033.py
@@ -1,29 +1,28 @@
-from sklearn import tree #memanggil library tree dari sklrearn
-import pandas as pd #memanggil library pandas
+from sklearn import tree 
+import pandas as pd 
 
 def prepoc(datapath):
-    nanas = pd.read_csv(datapath, sep=',') #menampung file csv dengan variable nanas
-    len(nanas) #mengjitung csv
+    nanas = pd.read_csv(datapath, sep=',') 
+    len(nanas) 
 
-    nanas = nanas.sample(frac=1) #memilih contoh
-    nanas_train = nanas[:201] #mengambil data training
-    nanas_test = nanas[200:] #mengambil data testing
+    nanas = nanas.sample(frac=1) 
+    nanas_train = nanas[:201] 
+    nanas_test = nanas[200:] 
 
-    nanas_train_att = nanas_train.drop(['blue'], axis=1) #memanggil baris blue pada csv pada training
-    nanas_train_pass = nanas_train['blue'] #memanggil baris blue pada csv trainig
+    nanas_train_att = nanas_train.drop(['blue'], axis=1) 
+    nanas_train_pass = nanas_train['blue'] 
     
-    nanas_test_att = nanas_test.drop(['blue'], axis=1) #memanggil data label blue pada csv training
-    nanas_test_pass = nanas_test['blue'] #memanggil data label blue pada csv training
+    nanas_test_att = nanas_test.drop(['blue'], axis=1) 
+    nanas_test_pass = nanas_test['blue'] 
     
-    nanas_att = nanas.drop(['blue'], axis=1) #memanggil data label blue pada csv training
-    nanas_pass = nanas['blue'] #memanggil data label blue pada csv training
-    return nanas_train_att,nanas_train_pass,nanas_test_att,nanas_test_pass,nanas_att,nanas_pass #mengembalikan nilai variable
+    nanas_att = nanas.drop(['blue'], axis=1) 
+    nanas_pass = nanas['blue'] 
+    return nanas_train_att,nanas_train_pass,nanas_test_att,nanas_test_pass,nanas_att,nanas_pass 
 
-
-def training(nanas_train_att, nanas_train_pass): #mengatur data training
-    apel = tree.DecisionTreeClassifier(criterion="entropy", max_depth=5) #mengatur data yang ditampilkan dan mengatur maksimal ketika di tampilkan
+def training(nanas_train_att, nanas_train_pass): 
+    apel = tree.DecisionTreeClassifier(criterion="entropy", max_depth=5) 
     apel = apel.fit(nanas_train_att, nanas_train_pass)
     return apel
 
-def testing(apel,testdataframe): #mengatur data testing
+def testing(apel,testdataframe): 
     return apel.predict(testdataframe)
--- test_app.py
+++ test_app.py
@@ -63,12 +63,12 @@ class TestApp(unittest.TestCase):
         self.assertLessEqual(result[0], 2)
 
     def test_02_rizal_1184033(self):
-        from Chapter01.rizalramadhan1184033 import prepoc, training, testing #mengimport fuction pada class rizalramadhan1184033
-        datapath = 'Chapter01/dataset/train.csv' #menampung file csv
+        from Chapter01.rizalramadhan1184033 import prepoc, training, testing
+        datapath = 'Chapter01/dataset/train.csv'
         nanas_train_att, nanas_train_pass, nanas_test_att, nanas_test_pass, nanas_att, nanas_pass = prepoc(datapath)
-        apel = training(nanas_train_att, nanas_train_pass) #memanggil attribut dari class yang di import
-        hasiltestingsemua = testing(apel, nanas_test_att) #menampilkan semua hasil testing
-        print('\n Hasil testing : ') #menampilkan hasil di codingan
-        print(hasiltestingsemua) #menampilkan hasil di codingan
-        ambilsatuhasiltesting = hasiltestingsemua[1] #menampilkan hasil 
-        self.assertLessEqual(ambilsatuhasiltesting, 2) #memanggil hasil
+        apel = training(nanas_train_att, nanas_train_pass) 
+        hasiltestingsemua = testing(apel, nanas_test_att) 
+        print('\n Hasil testing : ') 
+        print(hasiltestingsemua) 
+        ambilsatuhasiltesting = hasiltestingsemua[1] 
+        self.assertLessEqual(ambilsatuhasiltesting, 2) 
