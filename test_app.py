286e9d12f34a146b96a0403ee16b5cfc0e23db1a
YukiArdiansyah <yukiardiansyah321@gmail.com>
2021-03-19 10:42:45 +0700

    2021c Yuki

--- test_app.py
+++ test_app.py
@@ -84,3 +84,14 @@ class TestApp(unittest.TestCase):
         ambilsatuhasiltesting = hasiltestingsemua[1]
         self.assertLessEqual(ambilsatuhasiltesting, 8)
         
+    def test_02_yuki_1184053(self):
+        from Chapter01.yuki1184053 import prepoc,training,testing
+        dataset='Chapter01/dataset/bankruptcy_prediction.csv'
+        pencak_train_att,pencak_train_pass,pencak_test_att,pencak_test_pass,pencak_att,pencak_pass= prepoc(dataset)
+        silat = training(pencak_train_att,pencak_train_pass)
+        hasiltestingsemua =     testing(silat,pencak_test_att)
+        print('\n hasil testing : ')
+        print(hasiltestingsemua)
+        ambilsatuhasiltesting = hasiltestingsemua[0]
+        self.assertLessEqual(ambilsatuhasiltesting, 1)
+
