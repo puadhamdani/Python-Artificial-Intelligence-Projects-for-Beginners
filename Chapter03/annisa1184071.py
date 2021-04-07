7293026afc958a2a4b72fe8313bfdc8fc50f10b6
annisakhairani <annisakhairani212@gmail.com>
2021-04-07 15:59:58 +0700

    annisa1184071

---
+++ Chapter03/annisa1184071.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  7 11:02:24 2021
+
+@author: ASUS
+"""
+
+import pandas
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def preparation():
+    dfs = pandas.read_csv('Chapter01/dataset/tweet.csv', sep= ',')
+
+    
+    cv = CountVectorizer()
+    dfs = dfs.sample(frac=1)
+    dfs_train = dfs[:15000]
+    dfs_test = dfs[15000:]
+    dfs_train_attribute = cv.fit_transform(dfs_train['tweet'])
+    dfs_train_win = dfs_train['label']
+    dfs_test_attribute = cv.transform(dfs_test['tweet'])
+    dfs_test_win = dfs_test['label']
+    data = [[dfs_train_attribute,dfs_train_win], [dfs_test_attribute, dfs_test_win]]
+    return data
+
+def training(dfs_train_att, dfs_train_win):
+    t = RandomForestClassifier(n_estimators=100)
+    t = t.fit(dfs_train_att,dfs_train_win)
+    return t
+
+def testing(t, testdataframe):
+    return t.predict(testdataframe)
